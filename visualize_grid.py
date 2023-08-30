a3865c759ad300ef0514f33b78c379b176e1fccf
Nat Youngren <natahyoungren@gmail.com>
2023-08-30 14:00:32 -0400

    Finished implementing selection and expansion

--- visualize_grid.py
+++ visualize_grid.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pygame as pg
 
+# TODO: Move all constants to a separate file
+
 MANUAL_CONTROL = True
 
 SCREEN_W, SCREEN_H = 500, 500
 
-
 BORDER_PX = 1
-GRID_W, GRID_H = 25, 25
+GRID_W, GRID_H = 5, 5
 
 BG_COLOR = (0, 0, 0) # Black
 CELL_COLORS = (255, 240, 240), (240, 240, 255) # Light red, light blue
@@ -15,15 +16,17 @@ SEARCHED_COLORS = (240, 200, 200), (200, 200, 240) # Less light red, less light 
 FRONTIER_COLORS = (220, 255, 180), (180, 255, 220) # Green tinted red, green tinted blue
 WALL_COLOR = (15, 15, 15) # Dark gray
 
+START_COLOR = (100, 150, 100) # Dark green
+END_COLOR = (150, 100, 100) # Dark red
+
 DEFAULT_COST = 1
 WALL_COST = 255
 
 CELL_W, CELL_H = SCREEN_W / GRID_W, SCREEN_H / GRID_H
 
 
-COST_GRID = np.ones((GRID_W, GRID_H), dtype=np.uint8)
+COST_GRID = np.full((GRID_W, GRID_H), fill_value=DEFAULT_COST, dtype=np.uint8)
 TRAVEL_GRID = np.full((GRID_W, GRID_H), fill_value=np.inf, dtype=np.float32)
-# POS_QUEUE = {}
 
 
 # Heuristic distance from each tile to the end, (can be precomputed, but not necessary)
@@ -36,18 +39,10 @@ G_GRID = np.full((GRID_W, GRID_H), fill_value=np.inf, dtype=np.float32)
 F_GRID = np.full((GRID_W, GRID_H), fill_value=np.inf, dtype=np.float32)
 
 # Parent of each tile, used to reconstruct path, 1D array
-P_GRID = np.full((GRID_W * GRID_H), fill_value=np.nan, dtype=np.uint)
+P_GRID = np.full((GRID_W, GRID_H, 2), fill_value=-1, dtype=int)
 
-FRONTIER = np.zeros((GRID_W, GRID_H), dtype=np.uint)
-
-
-# START_POS = (0, 0)
-# START_POS = None
-START_COLOR = (100, 150, 100)
-
-# END_POS = (GRID_W-1, GRID_H-1)
-# END_POS = None
-END_COLOR = (150, 100, 100)
+# Holds status of each tile, 0 = unvisited, 1 = frontier, -1 = searched
+FRONTIER = np.zeros((GRID_W, GRID_H), dtype=int)
 
 
 
@@ -114,47 +109,103 @@ def main():
                 elif event.key == pg.K_r:
                     print('r')
                     return main()
-                
+            
             elif event.type == pg.MOUSEBUTTONDOWN:
                 clicked_tile = get_tile(event.pos)
                 print(clicked_tile)
                 if start_pos is None:
                     start_pos = clicked_tile
-                    # pg.draw.rect(screen, START_COLOR, (start_pos[0]*CELL_W, start_pos[1]*CELL_H, CELL_W, CELL_H))
-                    # POS_QUEUE[start_pos] = 0
-                    FRONTIER[start_pos] = 1
                     
                 elif end_pos is None:
                     end_pos = clicked_tile
-                    # pg.draw.rect(screen, END_COLOR, (end_pos[0]*CELL_W, end_pos[1]*CELL_H, CELL_W, CELL_H))
-                    
-                elif not searching:
-                    print(distance_heuristic(clicked_tile, end_pos))
-                    if COST_GRID[clicked_tile[0], clicked_tile[1]] == WALL_COST:
-                        COST_GRID[clicked_tile[0], clicked_tile[1]] = 1
-                    else:
+                    add_to_frontier(start_pos, end_pos)
+
+            if not searching and start_pos is not None and end_pos is not None: # Before searching, allow user to place walls
+                try:
+                    if pg.mouse.get_pressed()[0]:
+                        clicked_tile = get_tile(pg.mouse.get_pos())
                         COST_GRID[clicked_tile[0], clicked_tile[1]] = WALL_COST
-                    # pg.draw.rect(screen, WALL_COLOR, (clicked_tile[0]*CELL_W, clicked_tile[1]*CELL_H, CELL_W, CELL_H))
-                
+                    if pg.mouse.get_pressed()[2]:
+                        clicked_tile = get_tile(pg.mouse.get_pos())
+                        COST_GRID[clicked_tile[0], clicked_tile[1]] = DEFAULT_COST
+                        
+                except AttributeError:
+                    pass
         pg.display.flip()
+
 
 def distance_heuristic(pos, end_pos):
     return np.sqrt((pos[0] - end_pos[0])**2 + (pos[1] - end_pos[1])**2)
 
+
+def add_to_frontier(pos, end_pos, prev_pos=None):
+    
+    if pos[0] < 0 or pos[0] >= GRID_W or pos[1] < 0 or pos[1] >= GRID_H:
+        return
+    if COST_GRID[pos] == WALL_COST:
+        return
+    
+    if prev_pos is None:
+        g = 0
+    else:
+        g = G_GRID[prev_pos] + distance_heuristic(pos, prev_pos)*COST_GRID[pos] # TODO: Multiple tile cost by distance_heuristic to add extra diagonal cost
+    
+    # Technically we may be recalculating this value, but it's not a big deal
+    h = distance_heuristic(pos, end_pos)
+    f = g + h
+    print(pos, f, g, h)
+    if f < F_GRID[pos]:
+        F_GRID[pos] = f
+        G_GRID[pos] = g
+        H_GRID[pos] = h
+        # P_GRID[pos] = prev_pos
+        # NOTE: May need to update the entire chain of children, if one exists
+        FRONTIER[pos] = 1
+        
+        if prev_pos is not None:
+            P_GRID[pos] = prev_pos
+
+        # FRONTIER[prev_pos] = -1
+    if pos == end_pos:
+        print('Found end position')
+        return
+    
+
+def add_neighbors_to_frontier(pos, end_pos):
+    for w in range(-1, 2):
+        for h in range(-1, 2):
+            if w == 0 and h == 0:
+                continue
+            neighbor_pos = (pos[0] + w, pos[1] + h)
+            
+            
+            # if FRONTIER[neighbor_pos] == 0:
+            add_to_frontier(neighbor_pos, end_pos, pos)
+            
+
+
 def select_next_pos():
-    min_f_indexes = np.argwhere(F_GRID == np.min(F_GRID))
-    print(min_f_indexes)
-    min_f_h_index = np.argmin(H_GRID[min_f_indexes[:, 0], min_f_indexes[:, 1]])
-    print(min_f_h_index)
+    masked_f = np.ma.masked_where(FRONTIER != 1, F_GRID)
+    masked_h = np.ma.masked_where(masked_f != np.min(masked_f), H_GRID)
+    # print(masked_f, masked_f.shape)
+    # print(masked_h, masked_h.shape)
+    return_pos = np.argmin(masked_h)
+    return tuple(np.unravel_index(return_pos, H_GRID.shape))
+
     
     
 def step(end_pos):
+
     print('Stepping...')
     # Implement A* here
     if not any(FRONTIER.flatten() == 1):
         print('No more positions to check')
         return
     
+    p = select_next_pos()
+    print(p)
+    add_neighbors_to_frontier(p, end_pos)
+    FRONTIER[p] = -1
     # k_index = np.argmin(POS_QUEUE.values())
     # next_pos = list(POS_QUEUE.keys())[k_index]
     # print(next_pos)
@@ -163,7 +214,7 @@ def step(end_pos):
     
     
     
-
+# TODO: Consolidate all the draw functions into one
 def set_board(screen):
     for w in range(GRID_W):
         for h in range(GRID_H):
@@ -181,8 +232,7 @@ def draw_walls(screen):
             if COST_GRID[w, h] == WALL_COST:
                 pg.draw.rect(screen, WALL_COLOR, (w*CELL_W+BORDER_PX, h*CELL_H+BORDER_PX, CELL_W-BORDER_PX*2, CELL_H-BORDER_PX*2))
 
-def draw_path(screen):
-    pass
+
 
 def draw_search(screen):
     for w in range(GRID_W):
@@ -194,13 +244,19 @@ def draw_search(screen):
             # if TRAVEL_GRID[w, h] != np.inf:
             #     pg.draw.rect(screen, SEARCHED_COLORS[(h + w) % 2], (w*CELL_W+BORDER_PX, h*CELL_H+BORDER_PX, CELL_W-BORDER_PX*2, CELL_H-BORDER_PX*2))
                 
-def draw_frontier(screen):
-    for w in range(GRID_W):
-        for h in range(GRID_H):
-            if (w, h) in FRONTIER:
-                pg.draw.rect(screen, FRONTIER_COLORS[(h + w) % 2], (w*CELL_W+BORDER_PX, h*CELL_H+BORDER_PX, CELL_W-BORDER_PX*2, CELL_H-BORDER_PX*2))
+# def draw_frontier(screen):
+#     for w in range(GRID_W):
+#         for h in range(GRID_H):
+#             if (w, h) in FRONTIER:
+#                 pg.draw.rect(screen, FRONTIER_COLORS[(h + w) % 2], (w*CELL_W+BORDER_PX, h*CELL_H+BORDER_PX, CELL_W-BORDER_PX*2, CELL_H-BORDER_PX*2))
+
+def draw_path(screen):
+    pass
 
 def get_tile(pos):
+    pos = np.array(pos)
+    np.clip(pos[:1], 0, SCREEN_W-1, out=pos[:1])
+    np.clip(pos[0:], 0, SCREEN_H-1, out=pos[0:])
     return (int(pos[0] / CELL_W), int(pos[1] / CELL_H))
 
 if __name__ == '__main__':
