04670a9415d19aa501f0df5b020498e4495a67a1
NatYoungren <natahyoungren@gmail.com>
2023-09-05 10:46:18 -0400

    Added portal colors

--- display_vars.py
+++ display_vars.py
@@ -2,11 +2,15 @@ SCREEN_W, SCREEN_H = 800, 800
 BORDER_PX = 1
 
 BG_COLOR = (0, 0, 0) # Black, seen in grid lines between cells
-CELL_COLORS = (255, 255, 255), (255, 255, 255) 
+
+CELL_COLORS = (255, 255, 255), (255, 255, 255)
+
 TRAVERSED_COLORS = (200, 255, 200), (200, 255, 200)
 SEARCHED_COLORS = (100, 255, 100), (100, 255, 100) # Green tinted red, green tinted blue
-WALL_COLOR = (15, 15, 15) # Dark gray
 PATH_COLORS = (240, 50, 50), (200, 0, 0) # Red, darker red
+
+PORTAL_COLORS = (160, 0, 250), (0, 240, 250) # Purple entrance, cyan exit
+WALL_COLOR = (15, 15, 15) # Dark gray
 
 START_COLOR = (100, 180, 100) # Dark green
 END_COLOR = (180, 80, 180) # Dark purple
