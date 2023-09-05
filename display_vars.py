9c22947c00804dbba6408d4c3925fa69cd48def8
NatYoungren <natahyoungren@gmail.com>
2023-09-05 09:29:59 -0400

    moved display/color vars to separate

---
+++ display_vars.py
@@ -0,0 +1,13 @@
+SCREEN_W, SCREEN_H = 800, 800
+BORDER_PX = 1
+
+BG_COLOR = (0, 0, 0) # Black, seen in grid lines between cells
+CELL_COLORS = (255, 255, 255), (255, 255, 255) 
+TRAVERSED_COLORS = (200, 255, 200), (200, 255, 200)
+SEARCHED_COLORS = (100, 255, 100), (100, 255, 100) # Green tinted red, green tinted blue
+WALL_COLOR = (15, 15, 15) # Dark gray
+PATH_COLORS = (240, 50, 50), (200, 0, 0) # Red, darker red
+
+START_COLOR = (100, 180, 100) # Dark green
+END_COLOR = (180, 80, 180) # Dark purple
+
