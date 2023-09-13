b97cf5b3e6e7cb7031c6fe2b921c9fa445b712e5
NatYoungren <natahyoungren@gmail.com>
2023-09-13 15:55:43 -0400

    Added cost color gradient, made text blue

--- display_vars.py
+++ display_vars.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 # Display vars
-SCREEN_W, SCREEN_H = 800, 800
+SCREEN_W, SCREEN_H = 800, 600
 
 # Grid vars
 BORDER_PX = 1
@@ -15,12 +17,18 @@ PATH_COLORS = (240, 50, 50), (200, 0, 0) # Red, darker red
 PORTAL_COLORS = (160, 0, 250), (0, 240, 250) # Purple entrance, cyan exit
 WALL_COLOR = (15, 15, 15) # Dark gray
 
+COST_EXTREMES = (255, 255, 255), (80, 80, 80) # White, gray (for cost display)
+COST_COUNT = 9
+COST_COLORS = np.array([np.linspace(COST_EXTREMES[0][0], COST_EXTREMES[1][0], COST_COUNT),
+                        np.linspace(COST_EXTREMES[0][1], COST_EXTREMES[1][1], COST_COUNT),
+                        np.linspace(COST_EXTREMES[0][2], COST_EXTREMES[1][2], COST_COUNT)]).T.astype(int)
+
 START_COLOR = (100, 180, 100) # Dark green
 END_COLOR = (180, 80, 180) # Dark purple
 
 # Text vars
 TEXT_FONT = 'freesansbold.ttf'
-TEXT_COLOR = (100, 100, 100) # Gray
-TEXT_ALPHA = 180
+TEXT_COLOR = (100, 100, 200) # Blue
+TEXT_ALPHA = 230
 TEXT_OFFSET = (40, -20)
 TEXT_SIZE = 24
